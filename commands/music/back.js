c19a42532ba71cb87cb48d760af934df4843db9c
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-24 13:19:34 -0300

    Update back.js

--- commands/music/back.js
+++ commands/music/back.js
@@ -7,12 +7,12 @@ module.exports = {
     async execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`Nenhuma música em reprodução ${message.author}... tentar novamente? ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`❌ Nenhuma música em reprodução ${message.author}... tentar novamente? ❌`);
 
-        if (!queue.previousTracks[1]) return message.channel.send(`Não havia música tocada antes ${message.author}... tentar novamente? ❌`);
+        if (!queue.previousTracks[1]) return message.channel.send(`❌ Não havia música tocada antes ${message.author}... tentar novamente? ❌`);
 
         await queue.back();
 
-        message.channel.send(`Reproduzindo a música **anterior** ✅`);
+        message.channel.send(`✅ Reproduzindo a música **anterior** ✅`);
     },
 };
