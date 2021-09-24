bc9941c0003b9aabfe1fd8b95585150b31d69b21
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-24 13:19:11 -0300

    Update clear.js

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -7,12 +7,12 @@ module.exports = {
     async execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`❌ Nenhuma música tocando no momento ${message.author}... tentar novamente? ❌`);
 
-        if (!queue.tracks[0]) return message.channel.send(`No music in the queue after the current one ${message.author}... try again ? ❌`);
+        if (!queue.tracks[0]) return message.channel.send(`❌ Não há música na fila depois da atual ${message.author}... tentar novamente? ❌`);
 
         await queue.clear();
 
-        message.channel.send(`The queue has just been cleared 🗑️`);
+        message.channel.send(`🗑️ A fila acabou de ser limpa! 🗑️`);
     },
 };
