b8b7e461e7187184bcfcfa000a63e71ba0c1502b
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-24 18:55:42 -0300

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -9,20 +9,20 @@ module.exports = {
     execute(client, message, args) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`❌ Nenhuma música tocando no momento ${message.author}... tentar novamente? ❌`);
 
         if (args.join('').toLowerCase() === 'queue') {
-            if (queue.repeatMode === 1) return message.channel.send(`You must first disable the current music in the loop mode (${client.config.app.px}loop) ${message.author}... try again ? ❌`);
+            if (queue.repeatMode === 1) return message.channel.send(`❌ Você deve primeiro desativar a música atual do modo loop (${client.config.app.px}loop) ${message.author}... tentar novamente? ❌`);
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Repeat mode **${queue.repeatMode === 0 ? 'disabled' : 'enabled'}** the whole queue will be repeated endlessly 🔁` : `Something went wrong ${message.author}... try again ? ❌`);
+            return message.channel.send(success ? `🔁 Modo de repetição **${queue.repeatMode === 0 ? 'disabled' : 'enabled'}** toda a fila será repetida indefinidamente 🔁` : `❌ Algo deu errado ${message.author}... tentar novamente? ❌`);
         } else {
-            if (queue.repeatMode === 2) return message.channel.send(`You must first disable the current queue in the loop mode (${client.config.app.px}loop queue) ${message.author}... try again ? ❌`);
+            if (queue.repeatMode === 2) return message.channel.send(`❌ Você deve primeiro desabilitar a fila atual do loop (${client.config.app.px}loop queue) ${message.author}... tentar novamente? ❌`);
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.TRACK : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Repeat mode **${queue.repeatMode === 0 ? 'disabled' : 'enabled'}** the current music will be repeated endlessly (you can loop the queue with the <queue> option) 🔂` : `Something went wrong ${message.author}... try again ? ❌`);
+            return message.channel.send(success ? `🔂 Modo de repetição **${queue.repeatMode === 0 ? 'disabled' : 'enabled'}** a música atual será repetida indefinidamente (você pode repetir a fila com a opção <queue>) 🔂` : `❌ Algo deu errado ${message.author}... tentar novamente? ❌`);
         };
     },
 };
