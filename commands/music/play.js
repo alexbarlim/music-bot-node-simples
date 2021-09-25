b2dde48f68478f1b59c49b5bfdbfbf925ab2311f
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-25 12:07:48 -0300

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -7,14 +7,14 @@ module.exports = {
     voiceChannel: true,
 
     async execute(client, message, args) {
-        if (!args[0]) return message.channel.send(`Please enter a valid search ${message.author}... try again ? ❌`);
+        if (!args[0]) return message.channel.send(`❌ Por favor insira uma pesquisa válida ${message.author}... tentar novamente? ❌`);
 
         const res = await player.search(args.join(' '), {
             requestedBy: message.member,
             searchEngine: QueryType.AUTO
         });
 
-        if (!res || !res.tracks.length) return message.channel.send(`No results found ${message.author}... try again ? ❌`);
+        if (!res || !res.tracks.length) return message.channel.send(`❌ Nenhum resultado encontrado ${message.author}... tentar novamente? ❌`);
 
         const queue = await player.createQueue(message.guild, {
             metadata: message.channel
@@ -24,10 +24,10 @@ module.exports = {
             if (!queue.connection) await queue.connect(message.member.voice.channel);
         } catch {
             await player.deleteQueue(message.guild.id);
-            return message.channel.send(`I can't join the voice channel ${message.author}... try again ? ❌`);
+            return message.channel.send(`❌ Não consigo entrar no canal de voz ${message.author}... tentar novamente? ❌`);
         }
 
-        await message.channel.send(`Loading your ${res.playlist ? 'playlist' : 'track'}... 🎧`);
+        await message.channel.send(`🎧 Carregando sua ${res.playlist ? 'playlist' : 'track'}... 🎧`);
 
         res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
 
